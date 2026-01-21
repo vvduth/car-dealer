c0633e7cf28c659ff3cabd48d4fc5d070ff955f3
vvduth <ducthai060501@gmail.com>
2026-01-21 21:23:28 +0200

    fix: update import statement for PrismaPlugin and add type declaration

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from "next";
-const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 const nextConfig: NextConfig = {
   /* config options here */
   compress: true,
