2ae97d701f3e9381015215513884fcdeabf6750f
vvduth <ducthai060501@gmail.com>
2026-01-21 21:28:25 +0200

    fix: remove unused PrismaPlugin import and related webpack configuration

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from "next";
-import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
 const nextConfig: NextConfig = {
   /* config options here */
   compress: true,
@@ -11,12 +11,6 @@ const nextConfig: NextConfig = {
     authInterrupts: true,
   },
   turbopack: {},
-  webpack: (config, { isServer }) => {
-        if (isServer) {
-            config.plugins = [...config.plugins, new PrismaPlugin()];
-        }
-        return config;
-    },
   async headers() {
     return [
       {
